7f27dae56d71d92a0337f525ada1bbd6fd38b2ba
Artur Nikolaienko <nak.nikolaenkotm@gmail.com>
2022-06-23 22:09:10 +0300

    fix naming errors

--- ViaplaySection+CoreDataClass.swift
+++ ViaplaySection+CoreDataClass.swift
@@ -24,6 +24,6 @@ public class ViaplaySection: NSManagedObject {
   
   func configure(section: DTO.Section) {
     self.text = section.description
-    self.pageType = section.pageType
+    self.sectionType = section.pageType
   }
 }
