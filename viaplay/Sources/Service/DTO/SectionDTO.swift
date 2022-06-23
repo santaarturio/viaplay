86655e0a4cbb745474944386e01a42f7257c4958
Artur Nikolaienko <nak.nikolaenkotm@gmail.com>
2022-06-23 19:32:30 +0300

    extend section DTO for the 'pageType' field

--- viaplay/Sources/Service/DTO/SectionDTO.swift
+++ viaplay/Sources/Service/DTO/SectionDTO.swift
@@ -2,12 +2,13 @@ extension DTO {
   
   struct Section: Codable {
     let id: String
+    let pageType: String
     let title: String
     let description: String
     
     enum CodingKeys: String, CodingKey {
       case id = "sectionId"
-      case title, description
+      case pageType, title, description
     }
   }
 }
